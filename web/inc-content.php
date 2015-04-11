7d77450ca0b0fbf1561be1e1ccf9d676c528e00f
Eric Rasch <ericrasch@gmail.com>
2015-04-11 00:10:11 -0500

    Adding initial content

---
+++ web/inc-content.php
@@ -0,0 +1,11 @@
+<div class="page-intro">
+  <video src="VIDEO URL" autoplay loop class="page-intro-video"></video>
+  <span class="page-intro-title">
+    Eric Rasch
+  </span>
+</div>
+<div class="page-bd">
+  <div class="wrapper">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quibusdam harum culpa tenetur, labore repellendus illo eius, autem facilis fugit excepturi quaerat officiis libero beatae minima eveniet voluptas veritatis laudantium!
+  </div>
+</div>
---
+++ web/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="initial-scale=1">
+
+    <title>EricRasch.com -  I make websites. I take photos. I design things. I speak. I am superdad. Oh, and I solve problems.</title>
+
+    <?php require( dirname( __FILE__ ) . '/inc-header.php' ); ?>
+
+  </head>
+  <body>
+
+    <?php require( dirname( __FILE__ ) . '/inc-content.php' ); ?>
+
+  </body>
+</html>
