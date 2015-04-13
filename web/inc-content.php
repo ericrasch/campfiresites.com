62606979bb355cd5689c6cfb74d88ad3f62e765f
Eric Rasch <ericrasch@gmail.com>
2015-04-12 22:59:02 -0500

    Content update

--- web/inc-content.php
+++ web/inc-content.php
@@ -21,9 +21,6 @@
     <li>
       <a href="http://forrst.com/people/ericrasch" title="Follow Eric Rasch on Forrst" class="icon-forrst"><span>Forrst</span></a>
     </li>
-    <li>
-      <a href="http://bit.ly/ERvimeo" title="View videos from Eric Rasch on Vimeo" class="icon-vimeo"><span>Vimeo</span></a>
-    </li>
 <?php /*
     <li>
       <a href="https://speakerdeck.com/ericrasch" title="View Eric Rasch's presentations on SpeakerDeck" class="icon-speakerdeck"><span>SpeakerDeck</span></a>
@@ -36,6 +33,6 @@
 </header>
 <main>
   <p>
-    <strong>campfiresites.com</strong> is the savvy, web design studio of <a href="http://ericrasch.com">Eric Rasch</a>, specializing in designing and building effective, premium WordPress Themes and websites.
+    <strong>campfiresites.com</strong> is the savvy, web design studio of <a href="http://ericrasch.com">Eric Rasch</a>, specializing in designing and building effective, premium WordPress Themes and websites. If you're interested in things I've presented on, check out my <a href="https://speakerdeck.com/ericrasch" title="View Eric Rasch's presentations on SpeakerDeck">SpeakerDeck</a> or <a href="http://slidesha.re/ERslide" title="View Eric Rasch's presentations on SlideShare">SlideShare</a> profiles. If you need me for a project, feel free to contact me through any of the social channels above, however, I'm being <em>very</em> selective about what I work on these days.
   </p>
 </main>
