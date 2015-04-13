2879c474a6cbdb671c7ea99800fb2aae4d3de474
Eric Rasch <ericrasch@gmail.com>
2015-04-12 22:00:57 -0500

    Added 410 error page

---
+++ web/410.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="initial-scale=1">
+
+    <title>410 Content Removed - EricRasch.com</title>
+
+    <?php require( dirname( __FILE__ ) . '/inc-header.php' ); ?>
+
+  </head>
+  <body>
+
+    <?php require( dirname( __FILE__ ) . '/inc-content.php' ); ?>
+
+  </body>
+</html>
