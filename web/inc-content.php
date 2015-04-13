2d75797c46838281e5475ea2ae5a3be61e50daa1
Eric Rasch <ericrasch@gmail.com>
2015-04-12 22:00:36 -0500

    Changed content

--- web/inc-content.php
+++ web/inc-content.php
@@ -1,11 +1,50 @@
-<div class="page-intro">
-  <video src="VIDEO URL" autoplay loop class="page-intro-video"></video>
-  <span class="page-intro-title">
+<header>
+  <h1>
     Eric Rasch
-  </span>
-</div>
-<div class="page-bd">
-  <div class="wrapper">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quibusdam harum culpa tenetur, labore repellendus illo eius, autem facilis fugit excepturi quaerat officiis libero beatae minima eveniet voluptas veritatis laudantium!
-  </div>
-</div>
+  </h1>
+  <ul class="social-icons">
+    <li>
+      <a href="http://bit.ly/ertwit" title="Follow Eric Rasch on Twitter" class="icon-twitter"><span>Twitter</span></a>
+    </li>
+    <li>
+      <a href="http://bit.ly/erface" title="Friend Eric Rasch on Facebook" class="icon-facebook"><span>Facebook</span></a>
+    </li>
+    <li>
+      <a href="http://bit.ly/ERlink" title="Connect with Eric Rasch on LinkedIn" class="icon-linkedin"><span>LinkedIn</span></a>
+    </li>
+    <li>
+      <a href="https://github.com/ericrasch" title="Follow Eric Rasch on GitHub" class="icon-github"><span>GitHub</span></a>
+    </li>
+    <li>
+      <a href="http://bit.ly/erdrib" title="Follow Eric Rasch on Dribbble" class="icon-dribbble"><span>Dribbble</span></a>
+    </li>
+    <li>
+      <a href="http://slidesha.re/ERslide" title="View Eric Rasch's presentations on SlideShare" class="icon-slideshare"><span>SlideShare</span></a>
+    </li>
+    <li>
+      <a href="http://forrst.com/people/ericrasch" title="Follow Eric Rasch on Forrst" class="icon-forrst"><span>Forrst</span></a>
+    </li>
+    <li>
+      <a href="http://bit.ly/erflickr" title="Connect with Eric Rasch on Flickr" class="icon-flickr"><span>Flickr</span></a>
+    </li>
+    <li>
+      <a href="http://bit.ly/ERtube" title="View videos from Eric Rasch on YouTube" class="icon-youtube"><span>YouTube</span></a>
+    </li>
+    <li>
+      <a href="http://bit.ly/ERvimeo" title="View videos from Eric Rasch on Vimeo" class="icon-vimeo"><span>Vimeo</span></a>
+    </li>
+<?php /*
+    <li>
+      <a href="https://speakerdeck.com/ericrasch" title="View Eric Rasch's presentations on SpeakerDeck" class="icon-speakerdeck"><span>SpeakerDeck</span></a>
+    </li>
+    <li>
+      <a href="http://stackoverflow.com/users/284091/er" title="Solve problems with Eric Rasch on StackOverflow" class="icon-stackoverflow"><span>StackOverflow</span></a>
+    </li>
+*/ ?>
+  </ul>
+</header>
+<main>
+  <p>
+    I'm <strong>Eric Rasch</strong>, a web designer and developer located in hot, spacious Houston, Texas. I’ve been described as an idea guy, but I’m more than that. I find ways to make ideas work and get projects done. From the initial client meeting, concepting through the design stage, coding the project up to the launch and beyond... I provide solutions. I've also been known to <a href="http://bit.ly/erflickr"  title="Connect with Eric Rasch on Flickr">take photos</a>,  <a href="http://bit.ly/erdrib" title="Follow Eric Rasch on Dribbble">design things</a>, and <a href="https://speakerdeck.com/ericrasch" title="View Eric Rasch's presentations on SpeakerDeck" class="icon-speakerdeck">speak</a>. I am superdad to <strong>3 beautiful girls</strong> with the help of <strong>1 amazing wife</strong>.
+  </p>
+</main>
