68f0e4605f3290806adfbecbb46ac916ba2cfa1e
Eric Rasch <ericrasch@gmail.com>
2015-04-14 13:34:34 -0500

    Improved environment detection

--- web/inc-header.php
+++ web/inc-header.php
@@ -1,1 +1,14 @@
-<link rel="stylesheet" href="assets/dist/style.min.css">
+<?php
+
+  $whitelist = array('127.0.0.1', "::1");
+  if( in_array( $_SERVER['REMOTE_ADDR'], $whitelist ) ) {
+    define( 'PROD_ENV', 'local' );
+  }
+
+  if ( 'local' != PROD_ENV ) {
+    ?>
+  <link rel="stylesheet" href="http://campfiresites.com/assets/dist/style.min.css">
+  <?php } else { ?>
+  <link rel="stylesheet" href="assets/dev/style.css">
+
+<?php } ?>
